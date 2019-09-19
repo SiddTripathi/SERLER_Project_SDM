27e8e0ec10fda751c13d605f5bf3688b88d78a47
Siddharth <siddhart.tripathi@gmail.com>
2019-09-19 22:41:30 +1200

    Structure change

--- src/server.js
+++ src/server.js
@@ -57,18 +57,18 @@ const client = new Client({
   ssl: true
 });
 
-client.connect();
+// client.connect();
 
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
+// client.query(
+//   "SELECT table_schema,table_name FROM information_schema.tables;",
+//   (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//   }
+// );
 
 app.listen(8080, () => {
   console.log("Server is up on port 8080");
